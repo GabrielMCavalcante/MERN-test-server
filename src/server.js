0e97bc9b78025cbfa9e3858548b96a7be2fe2480
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2021-03-03 13:14:27 -0400

    Remove import for deleted connection file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const { routes } = require("./routes")
 const cors = require("cors")
-require("./database/connection")
 
 const app = express()
 const PORT = 3333
