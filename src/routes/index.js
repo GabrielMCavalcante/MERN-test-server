eac9dcc9f37d821c37b6541abb3eda7aba0de663
Gabriel Cavalcante <gabriel__xx@hotmail.com>
2021-03-03 10:20:59 -0400

    Add / route for testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const { PagesController } = require("../controllers/PagesController")
 
 const routes = Router()
 
+routes.get("/", (req, res) => {
+  return res.send("ok")
+})
 routes.get("/pages", PagesController.index)
 routes.post("/pages/:name", PagesController.create)
 
